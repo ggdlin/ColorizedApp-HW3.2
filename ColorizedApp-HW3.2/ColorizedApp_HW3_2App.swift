1e8f02efa647aaaaac433e06d8d19c289a199392
Sergey Ivanov <ggdlin@gmail.com>
2020-12-13 22:52:18 +0300

    Initial Commit

---
+++ ColorizedApp-HW3.2/ColorizedApp_HW3_2App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp_HW3_2App.swift
+//  ColorizedApp-HW3.2
+//
+//  Created by Ivanov Sergey on 13.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp_HW3_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorizedApp-HW3.2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorizedApp-HW3.2
+//
+//  Created by Ivanov Sergey on 13.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
